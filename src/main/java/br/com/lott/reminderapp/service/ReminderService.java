7d9f783762dc115863bb510f0492d0bea4b62327
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-10-10 22:29:52 -0300

    Imports organizing

--- src/main/java/br/com/lott/reminderapp/service/ReminderService.java
+++ src/main/java/br/com/lott/reminderapp/service/ReminderService.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class ReminderService {
